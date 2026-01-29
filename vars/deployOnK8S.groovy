f9c99fcae56968c469cc5cd4e96c877c9caa2be5
RedaEssawy <redaessawy81@gmail.com>
2026-01-29 22:45:28 +0200

    jenkins vars

--- vars/deployOnK8S.groovy
+++ vars/deployOnK8S.groovy
@@ -1,9 +1,12 @@
-def call(String API_SERVER, String SERVICEACCOUNT_TOKEN)  {
+def call(String API_SERVER, String SERVICEACCOUNT_TOKEN, String IMAGE_NAME, String DEPLOYMENT_FILE) { {
 
+     sh "sed -i 's|image: .*|image: ${IMAGE_NAME}:${BUILD_TAG}|' ${DEPLOYMENT_FILE}"
+             
         withCredentials([
                 string(credentialsId: 'APIServer', variable: 'API_SERVER'),
                 string(credentialsId: 'ServiceAccount-Token', variable: 'KUBE_TOKEN')
 
-                ]) 
-                sh "kubectl --server=${API_SERVER} --token=${KUBE_TOKEN} apply -f deployment.yaml  --insecure-skip-tls-verify=true  "
+                ]) {
+                sh "kubectl --server=${API_SERVER} --token=${KUBE_TOKEN} apply -f ${DEPLOYMENT_FILE}  --insecure-skip-tls-verify=true  "
+}
 }

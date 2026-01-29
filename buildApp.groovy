219367e25c8c996feaa4dccddb3e520770aa8b3b
RedaEssawy <redaessawy81@gmail.com>
2026-01-29 21:07:03 +0200

    jenkins vars

--- buildApp.groovy
+++ buildApp.groovy
@@ -1,4 +1,4 @@
-def buildApp() {
+def call() {
     sh "mvn test"
     sh "mvn package"
     // Add your build logic here
--- pushImage.groovy
+++ pushImage.groovy
@@ -1,4 +1,4 @@
-def pushImage(String IMAGE_NAME, String IMAGE_TAGC, String USERNAME, String PASSWORD) {
+def call(String IMAGE_NAME, String IMAGE_TAGC, String USERNAME, String PASSWORD) {
     sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
     sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
     // Add your image pushing logic here

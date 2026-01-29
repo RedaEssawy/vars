0b60b001f0b66c38dfa631cd99c8d8e0e688bbd8
RedaEssawy <redaessawy81@gmail.com>
2026-01-29 22:14:12 +0200

    jenkins vars

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -4,6 +4,6 @@ def call(String IMAGE_NAME, String DOCKER_CREDENTIALS_ID)  {
         sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
         sh "docker push ${IMAGE_NAME}:${BUILD_TAG}"
     }
-    sh "docker rmi ${IMAGE_NAME}:${BUILD_TAG}"
+
     // Add your image building logic here
 }

d065eaa351062e09ee75f26689eb47cd778ce8f0
RedaEssawy <redaessawy81@gmail.com>
2026-01-30 06:09:07 +0200

    Jenkins Shared Library

--- vars/deployOnK8S.groovy
+++ vars/deployOnK8S.groovy
@@ -2,7 +2,8 @@ def call(String API_SERVER,  String IMAGE_NAME, String DEPLOYMENT_FILE) {
 
      sh "sed -i 's|image: .*|image: ${IMAGE_NAME}:${BUILD_NUMBER}|' ${DEPLOYMENT_FILE}"
              
-        withCredentials([string(credentialsId: SERVICEACCOUNT_TOKEN, variable: 'SERVICEACCOUNT_TOKEN')]) {
+        withCredentials([string(credentialsId: ServiceAccount-Token, variable: 'SERVICEACCOUNT_TOKEN'),
+                         string(credentialsId: APIServer, variable: 'API_SERVER')]) {
             sh "kubectl --server=${API_SERVER} --token=${SERVICEACCOUNT_TOKEN} get pods --insecure-skip-tls-verify=true"
         }
      
